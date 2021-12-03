2d5f905ee89fdb4b508367ae8fabcc2b576a2fae
mme.matos.elise@gmail.com <elise.matos@hotmail.fr>
2021-12-03 14:36:18 +0100

    Initial commit

---
+++ src/main/java/com/application/knitting/model/Pattern.java
@@ -0,0 +1,29 @@
+package com.application.knitting.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pattern {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer numberOfRows;
+    private Integer numberOfStitches;
+    private String yarn;
+    private String description;
+    private String instructions;
+    private Instant updated;
+    private Instant created;
+}
---
+++ src/test/java/com/application/knitting/KnittingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.knitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnittingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
